9e7912e071c8861e037842abecd373fdb94bd412
yashwanthnaikbhukya <yashwanthnaikbhukya18@gmail.com>
2024-06-11 17:08:58 +0530

    modified frontend

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -48,27 +48,28 @@ const Notes = () =>{
                 <form className='my-3' >
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" id="etitle" name="etitle" aria-describedby="emailHelp" value={note.etitle} onChange={onChange}/>
+                        <input type="text" className="form-control" id="etitle" name="etitle" aria-describedby="emailHelp" value={note.etitle} onChange={onChange} minLength={5} required />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label">Decription</label>
-                        <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} />
+                        <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} minLength={5} required />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="tag" className="form-label">Tag</label>
-                        <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} />
+                        <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} minLength={3} required />
                     </div>
                 </form>
                 </div>
                 <div className="modal-footer">
                     <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary" onClick={handleClick}>Update Note</button>
+                    <button disabled={note.etitle.length < 5 || note.edescription.length < 5 } type="button" className="btn btn-primary" onClick={handleClick}>Update Note</button>
                 </div>
                 </div>
             </div>
             </div>
-            <div className="row my-3">
+            <div className="container row my-3">
             <h2>Your Notes</h2>
+            {notes.length === 0 && <h3 className='text-center my-4'><i class="fa-solid fa-magnifying-glass"></i> No notes to display</h3>}
             {notes.map((note) =>{
             return <Noteitem key={note._id} note={note} updateNote={updateNote} />
             })}
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -32,18 +32,7 @@ const NoteState = (props) => {
       body: JSON.stringify({title, description, tag})
     });
     // eslint-disable-next-line
-    const json = response.json();
-    //Logic
-    //console.log("added new note");
-    const note = {
-      "_id": "6666dd9ef7bbvd9xs3d46f650fsd1",
-      "user": "66649bd21e7507dv243e6955d7",
-      "title": title,
-      "description": description,
-      "tag": tag,
-      "date": "2024-06-10T11:03:58.660Z",
-      "__v": 0
-    };
+    const note = await response.json();
     setNotes(notes.concat(note))
   }
 
@@ -57,8 +46,8 @@ const NoteState = (props) => {
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjY2NDliZDIxZTc1MDcyNDNlNjk1NWQ3In0sImlhdCI6MTcxNzkyODE3MX0.bBvMM-oq3hwHMoA3eqRz47tuZ-XLsyWqmTgithNVwRE"
       }
     });
-    const json = response.json();
-    console.log(json)
+    // eslint-disable-next-line
+    const json = await response.json();
     //Logic
     const newNotes = notes.filter((note) => {return note._id !== id})
     setNotes(newNotes)
